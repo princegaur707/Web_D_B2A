8fc1511b4ec91309bbc518ca5bb6ec029a9f522e
princegaur707 <princegaur1998@gmail.com>
2023-01-22 23:55:30 +0530

    files changed

--- 13/index.js
+++ 13/index.js
@@ -11,25 +11,59 @@ const refreshList = function(){
         toDoItem.setAttribute('class',"list-item")
         toDoItem.innerText=arr[i];
         toDoItem.innerHTML +='<span class="deleteItem">&#10060;</span>'//adding cross sign
+        toDoItem.innerHTML +='<span class="editItem">&#x1F589</span>'//adding cross sign
+        // console.log(toDoItem.innerText);
         listEI.insertAdjacentElement('beforeend',toDoItem);
     }
+    DeleteFunction();//whenever new element in list is added firstly every element from HTML array(not original array)
+    //is deleted so we need to add again DeleteFunction to every element
 }
 
 submitinputE1.addEventListener('keyup', (e)=>{
     // console.log(e.keyCode)  to know what is the key code of any key
-    if(e.keyCode == 13){
+    if(e.keyCode === 13){
         const val = e.target.value;//the text we typed will be stored in val
         e.target.value = "";//once Enter key is pressed we want our submit area to be clear
-       arr.push(val);
-       refreshList();
+        arr.push(val);
+        refreshList();
     }  
 })
-const del = Array.from(document.getElementsByClassName('deleteItem'));//as we have to apply 
-//on all so converted to array
-for(let i=0; i <del.length; i++){
-    del[i].addEventListener('click',(e)=>{
-        const item = e.target.parentNode;
-        const parent = item.parentNode;
-        parent.indexOf();
-    })
+refreshList()
+const editTodo = function(item, toEdit){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === toDoItem){
+            console.log(toDoItem)
+        }
+    }
 }
+
+function DeleteFunction(){
+    const deleteItemEls = Array.from(document.getElementsByClassName('deleteItem'));//Array.from lets
+    //to make the array from iterable objects to get every element with the help of for loop
+    // console.log("changed");
+    // console.log(deleteItemEls);
+    const editItemEls = Array.from(document.getElementsByClassName('editItem'));
+    // console.log(editItemEls);
+    for(let i=0; i <deleteItemEls.length; i++){
+        deleteItemEls[i].addEventListener('click',(e)=>{
+            const item = e.target.parentNode.innerText;
+            const toDoItem = item.slice(0, -1).trim(); //I am getting cross sign so used slice to remove that
+            // and trim is used to remove the space coming along with text 
+            arr.pop(arr.indexOf(toDoItem));
+            refreshList();
+        })
+        editItemEls.addEventListener('click', (e) =>{
+            const item = e.target.parentNode.innerText;
+            const toDoItem = item.slice(0, -3).trim();//cross sign, edit sign were coming with it
+            const val = submitinputE1.value;
+            if(val === "")
+            {
+                alert("Enter the value in the input Box")
+                return;
+            }
+            // editItemEls(toDoItem, val)
+            console.log(toDoItem,val);
+        } )
+    }
+}
+ 
